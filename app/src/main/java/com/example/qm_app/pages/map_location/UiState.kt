a94b6f38e890abbb2a39d6e985eadef9d83c40f1
shenxuxiang <shen_xuxiang@163.com>
2026-02-03 22:21:47 +0800

    feat: 导航页面添加poi检索功能

--- app/src/main/java/com/example/qm_app/pages/map_location/UiState.kt
+++ app/src/main/java/com/example/qm_app/pages/map_location/UiState.kt
@@ -3,6 +3,7 @@ package com.example.qm_app.pages.map_location
 import androidx.compose.runtime.Stable
 import com.amap.api.location.AMapLocation
 import com.amap.api.maps.model.Marker
+import com.amap.api.services.core.PoiItemV2
 import com.example.qm_app.pages.map_location.components.AMapView
 
 @Stable
@@ -11,4 +12,5 @@ data class UiState(
     val initialZoom: Float = 18f,
     val aMapView: AMapView? = null,
     val userLocation: AMapLocation? = null,
+    val poiList: List<PoiItemV2> = emptyList() // POI 集合
 )
